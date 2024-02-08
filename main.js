d1f2d48ef2f6c573df39d758d1016beef366d99a
gudzilla <khassan23.95@gmail.com>
2024-02-08 08:57:58 +0300

    Fixed styles, CSS classes names, color variables and more

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-const menuButton = document.querySelector('#mobileMenuButton');
-const headerNav = document.querySelector('#headerNav');
-// const menuLinks = document.querySelector('#navbarList');
+const menuButton = document.querySelector("#mobileMenuButton");
+const headerNav = document.querySelector("#headerNav");
 
-menuButton.addEventListener('click', function () {
-  menuButton.classList.toggle('mobile-menu-button_active');
-  headerNav.classList.toggle('navbar_mobile');
-})
+menuButton.addEventListener("click", function () {
+  menuButton.classList.toggle("mobile-menu-button_active");
+  headerNav.classList.toggle("navbar_mobile");
+});
